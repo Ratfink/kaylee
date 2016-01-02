703d2e9fb3033d4d8eeeb30976c2b4fff399f324
Clayton G. Hobbs <clay@lakeserv.net>
2016-01-02 13:34:50 -0500

    Update copyright for 2016

--- languageupdater.py
+++ languageupdater.py
@@ -1,6 +1,6 @@
 # This is part of Kaylee
 # -- this code is licensed GPLv3
-# Copyright 2015 Clayton G. Hobbs
+# Copyright 2015-2016 Clayton G. Hobbs
 # Portions Copyright 2013 Jezra
 
 import hashlib
